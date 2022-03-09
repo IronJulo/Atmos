a280ba45d2def4a2130ea0e00b7cf0f047fa24bb
IronJulo <jules.brss@gmail.com>
2022-03-10 00:15:59 +0100

    [EDIT] Rename salveNode to collector

--- web/backend/app/controllers/collector.controller.js
+++ web/backend/app/controllers/collector.controller.js
@@ -2,6 +2,6 @@ const db = require("../models");
 const Op = db.Sequelize.Op;
 
 exports.get = (req, res) => {
-    res.status(501).send({ message: "Slave node not implemented yet" });
+    res.status(501).send({ message: "Collectors not implemented yet" });
     return;
 };
